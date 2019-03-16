7212120cef71736758290906da3fc68f52fb22d4
adam kowalczyk <kombajno678@gmail.com>
2019-03-16 21:53:59 +0100

    zad 5 done

--- zadanie5/src/Main.java
+++ zadanie5/src/Main.java
@@ -1,87 +1,80 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.io.*;
 import java.util.Vector;
+
 class WektoryRoznejDlugosciException extends Exception{
-    WektoryRoznejDlugosciException(int len1, int len2){
-        System.out.println("error: vector length mismatch: " + len1 + " vs " + len2);
+    WektoryRoznejDlugosciException(String msg){
+        super(msg);
     }
 }
 public class Main {
-
-    public static void main(String[] args) throws WektoryRoznejDlugosciException{
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Hello World!");
-        Vector<Integer> vec1 = new Vector<>(0, 1);
+    public static Vector<Integer> getNewVector(){
+        Vector<Integer> vec = new Vector<>(10, 10);
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String line = null;
+
         try{
             line = input.readLine();
         }catch(Exception e){
             System.out.println(e.getStackTrace());
         }
         String[] strs = line.trim().split("\\s+");
-        try {
-            for (int i = 0; i < strs.length; i++) {
-                vec1.add(Integer.parseInt(strs[i]));
+
+        for (int i = 0; i < strs.length; i++) {
+            int temp;
+            try {
+                temp = Integer.parseInt(strs[i]);
+            }catch(Exception e){
+                continue;
             }
-        }catch(Exception e){
-            System.out.println(e.getStackTrace());
+            vec.add(temp);
         }
-        System.out.println(vec1.capacity());
-
-        Vector<Integer> vec2 = new Vector<>(0, 1);
-        try{
-            line = input.readLine();
-        }catch(Exception e){
-            System.out.println(e.getStackTrace());
-        }
-        strs = line.trim().split("\\s+");
-        try {
-            for (int i = 0; i < strs.length; i++) {
-                vec2.add(Integer.parseInt(strs[i]));
-            }
-        }catch(Exception e){
-            System.out.println(e.getStackTrace());
-        }
-        System.out.println(vec2.capacity());
-        try{
-            if(vec1.capacity() != vec2.capacity()){
-                //throw
-
-                throw new WektoryRoznejDlugosciException(vec1.capacity(), vec2.capacity());
-                //ask user to input again
-            }else{
-                //save sum to file
-                //Create("io.txt");
-
-                String path = "io.txt";
-                File f = new File(path);
-                try{
-                    f.createNewFile();
-                }catch(Exception e){
-                    System.out.println(e.getMessage());
+        return vec;
+    }
+    public static void main(String[] args){
+        Boolean flag = false;
+        do {
+            System.out.println("input two vector of equal length: ");
+            Vector<Integer> vec1 = getNewVector();
+            Vector<Integer> vec2 = getNewVector();
+            try {
+                if (vec1.size() != vec2.size()) {
+                    throw new WektoryRoznejDlugosciException("error: vector length mismatch: "+vec1.size()+"vs"+vec2.size());
+                } else {
+                    flag = true;
+                    //calculate sum
+                    Vector<Integer> sum = new Vector(vec1.size());
+                    for (int i = 0; i < sum.capacity(); i++) {
+                        sum.add(vec1.get(i) + vec2.get(i));
+                    }
+                    System.out.println("sum = " + sum.toString());
+                    //create file
+                    String path = "sum.txt";
+                    File f = new File(path);
+                    try {
+                        f.createNewFile();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    //save to file
+                    FileOutputStream out = null;
+                    PrintStream file = null;
+                    try {
+                        out = new FileOutputStream(f);
+                        file = new PrintStream(out);
+                        file.print(sum.toString());
+                        if (out != null) out.close();
+                        if (file != null) file.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                    System.out.println("sum saved to file: " + path);
                 }
-
-                FileOutputStream out = null;
-                try{
-                    out = new FileOutputStream(f);
-                    PrintStream file = new PrintStream(out);
-                    file.print(vec1.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally{
-                    //if(out != null)out.close();
-                    //if(file!= null)file.close();
-                }
-
+            } catch (WektoryRoznejDlugosciException e) {
+                System.out.println(e.getMessage());
             }
-
-        }catch(WektoryRoznejDlugosciException e){
-            System.out.println(e.getMessage());
-        }
-
+        }while(flag == false);
     }
 }
