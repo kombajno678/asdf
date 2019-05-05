b1253b7adb4b8916aa30d3b000b6f0e8eabbc256
adam kowalczyk <kombajno678@gmail.com>
2019-05-05 19:08:45 +0200

    zad8 +

--- new/src/zad8_client.java
+++ new/src/zad8_client.java
@@ -1,3 +1,63 @@
-public class zad8_client {
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class zad8_client {
+    public static void main(String[] args) throws Exception {
+        String ip="", msg="", datetime = "";
+        final int port = 55555;
+        var scanner = new Scanner(System.in);
+        //set servers ip address
+        if (args.length < 1) {
+            System.err.println("input servers ip address:");
+            if (scanner.hasNextLine()) {
+                ip = scanner.nextLine();
+            }
+        }else {
+            ip = args[0];
+        }
+        //set msg and datetime from arguments
+        if(args.length == 3){
+            msg = args[1];
+            datetime = args[2];
+        }
+        try (var socket = new Socket(ip, port)) {
+            System.out.println("Enter lines of text then Ctrl+D or Ctrl+C to quit");
+
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+
+
+            if(msg.isEmpty()) {
+                System.out.println("input text of notification:");
+                if (scanner.hasNextLine()) {
+                    msg = scanner.nextLine();
+                }
+            }
+            out.println(msg);
+
+            if(datetime.isEmpty()){
+                System.out.println("input date and time of notification: (format: \"yyyy-mm-dd- hh:mm\")");
+                if(scanner.hasNextLine()){
+                    datetime = scanner.nextLine();
+                }
+            }
+            out.println(datetime);
+
+            while(true){
+                if (in.hasNextLine()) {
+                    System.out.println("server> "+in.nextLine());
+                }
+                if (scanner.hasNextLine()) {
+                    String next = scanner.nextLine();
+                    if (next.matches("exit")){
+                        break;
+                    }
+                    out.println(scanner.nextLine());
+                }
+            }
+        }
+    }
 }
--- new/src/zad8_server.java
+++ new/src/zad8_server.java
@@ -2,34 +2,33 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.ParseException;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.*;
 class Notification{
-
     Calendar date = Calendar.getInstance();
     String text;
-
     Notification(String d, String t){
-        //"yyyy-mm-dd hh:mm"
-        Integer year = Integer.parseInt(d.substring(0,3));
-        Integer month = Integer.parseInt(d.substring(4,5));
-        Integer day = Integer.parseInt(d.substring(6,7));
-        Integer hour = Integer.parseInt(d.substring(8,9));
-        Integer minute = Integer.parseInt(d.substring(10,11));
-        date.set(year, month, day, hour, minute);
+        //"yyyy?MM?dd?hh?mm"
+        int year = Integer.parseInt(d.substring(0,4));
+        int month = Integer.parseInt(d.substring(5,7));
+        int day = Integer.parseInt(d.substring(8,10));
+        int hour = Integer.parseInt(d.substring(11,13));
+        int minute = Integer.parseInt(d.substring(14,16));
+        //System.out.println("... y" + year+ " m" + month+ " d" + day+ " h" + hour+ " m" + minute + " ...");
+        date.set(year, month-1, day, hour, minute, 0);
         text = t;
     }
     @Override public String toString(){
-        return text + " at: " + date.toString();
+        return "\""+text+"\"" + " @ " + date.getTime().toString();
     }
-
 }
 
 public class zad8_server {
     public static void main(String[] args) throws Exception{
         try(var listener = new ServerSocket(55555)){
-            System.out.println("server is running ...");
+            System.out.println("hello, notification server is up and running ...");
             var pool = Executors.newFixedThreadPool(20);
             while(true){
                 pool.execute(new Connection(listener.accept()));
@@ -43,39 +42,48 @@ public class zad8_server {
         }
         @Override
         public void run(){
-            System.out.println("connected: " + socket);
+            System.out.println(socket.getPort() +"> connected: " + socket);
             try{
                 var in = new Scanner(socket.getInputStream());
                 var out = new PrintWriter(socket.getOutputStream(), true);
-                while(in.hasNextLine()){
-                    //out.println(in.nextLine().toUpperCase());
-                    //get text
-                    String text = in.nextLine();
-                    String date = in.nextLine();
-                    Notification n = new Notification(date, text);
-                    out.println(n);
-                    //wait till date
-                    //send text
-                    //close
+                //out.println("you are now connected to server");
 
+                String text = "", date = "";
+                //get text
+                if(in.hasNextLine()){
+                    text = in.nextLine();
                 }
+                //get date
+                if(in.hasNextLine()){
+                    date = in.nextLine();
+                }
+                //create notification
+                Notification n = new Notification(date, text);
+                System.out.println(socket.getPort() + "> notification created: " + n);
+
+                //wait till datetime from notification
+                long waitTimeSeconds = (n.date.getTimeInMillis() - Calendar.getInstance().getTimeInMillis())/1000;
+                System.out.println(socket.getPort() + "> going to sleep for "+waitTimeSeconds+"s");
+                Thread.sleep(waitTimeSeconds*1000);
+
+                //send text from notification
+                out.println(n.text);
+                System.out.println(socket.getPort() + "> notification text sent");
+
+                //close
             }catch(Exception e){
-                System.out.println("error " + socket);
-                System.out.println(e.getMessage());
+                System.out.println(socket.getPort() +"> error " + socket);
+                System.out.println(socket.getPort() +"> "+e.getMessage());
             } finally{
                 try{
                     socket.close();
                 }catch(IOException e){
                     //caught
+                    System.out.println(socket.getPort() +"> "+e.getMessage());
                 }finally{
-                    System.out.println("closed " + socket);
+                    System.out.println(socket.getPort() +"> closed");
                 }
             }
-
-
         }
-
-
     }
-
 }
