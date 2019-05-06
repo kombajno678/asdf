ce5b3eb1d0780dd72f6ea50bd3a54bfabc842391
adam k <kombajno678@gmail.com>
2019-05-06 19:38:39 +0200

    wewr

--- new/src/zad8_client.java
+++ new/src/zad8_client.java
@@ -5,7 +5,26 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class zad8_client {
-    Boolean validateDateString(String date){
+    static Boolean validateDateString(String date){
+        String pattern = "20\\d\\d[-.\\/\\\\ ][01]\\d[-.\\/\\\\ ][0-3]\\d[ ][0-2]\\d[:][0-5]\\d";
+        if(date.matches(pattern)) {
+            int year = Integer.parseInt(date.substring(0,4));
+            int month = Integer.parseInt(date.substring(5,7));
+            int day = Integer.parseInt(date.substring(8,10));
+            int hour = Integer.parseInt(date.substring(11,13));
+            int minute = Integer.parseInt(date.substring(14,16));
+            if(year < 2019)return false;
+            if(month < 1 || month > 12)return false;
+            if(day < 1 || day > 31)return false;
+            if(hour < 0 || hour > 23)return false;
+            if(minute < 0 || minute > 59)return false;
+            if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                    && day > 31 )return false;
+            if((month == 4 || month == 6 || month == 9 || month == 11)&& day > 30) return false;
+            //TODO: leap years
+            if(month == 2 && day > 28)return false;
+            return true;
+        }
         return false;
     }
     public static void main(String[] args) throws Exception {
@@ -47,7 +66,13 @@ public class zad8_client {
                     datetime = scanner.nextLine();
                 }
             }
-            out.println(datetime);
+            if(validateDateString(datetime)){
+                out.println(datetime);
+            }else{
+                //throw exception or sth
+                //abort
+            }
+
 
             while(true){
                 if (in.hasNextLine()) {
